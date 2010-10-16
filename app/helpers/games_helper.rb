7bc29bf7dba32c059e1aa7f810f46941dc902c33
Peter Gebhard <pgebhard@gmail.com>
2010-10-15 23:27:52 -0400

    Stub-type usage of Hpricot for game stat parsing from Yahoo Sports.

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,10 @@
+require 'hpricot'
+require 'open-uri'
+
 module GamesHelper
+  def update_games
+    doc = open("http://rivals.yahoo.com/ncaa/football/scoreboard") { |f| Hpricot(f) }
+    elements = doc.search("//div[@class='ysp-dynamic']")
+  end
+  
 end
